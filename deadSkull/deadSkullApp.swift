16985aa04840734b3fc5c3d54d2e32092afec7ab
Telha Wasim <telhawasim@gmail.com>
2024-07-13 12:40:12 +0500

    first commit

---
+++ deadSkull/deadSkullApp.swift
@@ -0,0 +1,17 @@
+//
+//  deadSkullApp.swift
+//  deadSkull
+//
+//  Created by Telha Wasim on 13/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct deadSkullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
